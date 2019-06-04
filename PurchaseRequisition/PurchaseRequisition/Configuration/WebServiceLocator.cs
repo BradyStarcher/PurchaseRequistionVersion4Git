fce144f06ebf5a94e7dbcdf4705af55580ffdda4
Samantha <sburkey@wvup.edu>
2019-06-04 15:28:05 -0400

    Configuration files and most WebApiCall files done.

---
+++ PurchaseRequisition/PurchaseRequisition/Configuration/WebServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace PurchaseRequisition.Configuration
+{
+    public class WebServiceLocator : IWebServiceLocator
+    {
+        public WebServiceLocator(IConfiguration config)
+        {
+            var customSection = config.GetSection(nameof(WebServiceLocator));
+            ServiceAddress = customSection?.GetSection(nameof(ServiceAddress))?.Value;
+        }
+
+        public string ServiceAddress { get; }
+    }
+}
---
+++ PurchaseRequisition/PurchaseRequisition/WebServiceAccess/Base/IWebApiCalls.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PurchaseRequisition.Models;
+using PurchaseRequisition.ViewModels;
+
+namespace PurchaseRequisition.WebServiceAccess.Base
+{
+    public interface IWebApiCalls
+    {
+        Task<User> GetUserAsync(string userId);
+        Task<string> CreateUserAsync(string password, User user);
+        Task<string> UpdateUserAsync(string userId, User user);
+        Task DeleteUserAsync(string userId);
+        Task<string> ChangePasswordAsync(string userId, User user, string currPassword, string newPassword);
+    }
+}
