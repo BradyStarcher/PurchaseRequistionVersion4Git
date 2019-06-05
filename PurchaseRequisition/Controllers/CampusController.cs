63077a4a9dc68a3d1aefee3faeeee0ba5da0bb35
BradyStarcher <bradystarcher@hotmail.com>
2019-06-05 15:48:31 -0400

    Updated some Controllers

---
+++ PurchaseRequisition/Controllers/CampusController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PurchaseRequisition.Data;
+using PurchaseRequisition.Models;
+
+namespace PurchaseRequisition.Controllers
+{
+    public class CampusController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CampusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Campus
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Campus.Include(c => c.Address);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Campus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // GET: Campus/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressID"] = new SelectList(_context.Address, "ID", "City");
+            return View();
+        }
+
+        // POST: Campus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,AddressID,Active")] Campus campus)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(campus);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressID"] = new SelectList(_context.Address, "ID", "City", campus.AddressID);
+            return View(campus);
+        }
+
+        // GET: Campus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus.FindAsync(id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressID"] = new SelectList(_context.Address, "ID", "City", campus.AddressID);
+            return View(campus);
+        }
+
+        // POST: Campus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,AddressID,Active")] Campus campus)
+        {
+            if (id != campus.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(campus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CampusExists(campus.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressID"] = new SelectList(_context.Address, "ID", "City", campus.AddressID);
+            return View(campus);
+        }
+
+        // GET: Campus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // POST: Campus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var campus = await _context.Campus.FindAsync(id);
+            _context.Campus.Remove(campus);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CampusExists(int id)
+        {
+            return _context.Campus.Any(e => e.ID == id);
+        }
+    }
+}
--- PurchaseRequisition/Data/ApplicationDbContext.cs
+++ PurchaseRequisition/Data/ApplicationDbContext.cs
@@ -33,5 +33,15 @@ namespace PurchaseRequisition.Data
         }
 
         private string connectionString = @"Server=localhost\SQLEXPRESS;database=CapstoneVersionFour;MultipleActiveResultSets=true;Trusted_Connection=True;";
+
+        public DbSet<PurchaseRequisition.Models.Address> Address { get; set; }
+
+        public DbSet<PurchaseRequisition.Models.Campus> Campus { get; set; }
+
+        public DbSet<PurchaseRequisition.Models.College> College { get; set; }
+
+        public DbSet<PurchaseRequisition.Models.Division> Division { get; set; }
+
+        public DbSet<PurchaseRequisition.Models.Department> Department { get; set; }
     }
 }
